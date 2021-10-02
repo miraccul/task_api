586af06caa971d58bad7616e9e07352dbea32bf9
Vlad <balashovv.vlad@gmail.com>
2021-10-03 00:48:54 +0300

    fix problem

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,6 @@ class ProjectsController < ApplicationController
     skip_before_action :verify_authenticity_token
     def index
         @categories = Category.all
-        render json: @categories.map {|i| {id: i.id, title: i.title, todos: Task.where(category_id: i.id).select("id","text","isComplited")}}
+        render json: @categories.map {|i| {id: i.id, title: i.title, todos: Task.where(category_id: i.id).select("id","text","isComplited").order("id")}}
     end
 end
